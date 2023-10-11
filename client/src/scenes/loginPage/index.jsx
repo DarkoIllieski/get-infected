72122b24e7ab6719b579c4c6f35338d597fe21e9
Darko <dj_mamlyooo@hotmail.com>
2023-10-11 14:08:15 +0200

    LoginPage done!

--- client/src/scenes/loginPage/index.jsx
+++ client/src/scenes/loginPage/index.jsx
@@ -1,11 +1,33 @@
-import React from 'react'
-
+import { Box, typography, useTheme, useMediaQuery } from "@mui/system";
+import { Form } from "./Form";
 function LoginPage() {
+  const theme = useTheme();
+  const isNonMobileScreens = useMediaQuery("(min-width: 1000px)");
   return (
-    <div>
-      
-    </div>
-  )
+    <Box>
+      <Box
+        width="100%"
+        backgroundColor={theme.palette.background.alt}
+        p="1rem 6%"
+        textAlign="center"
+      >
+        <Typography fontWeight="bold" fontSize="32px" color="primary">
+          .getInfected
+        </Typography>
+      </Box>
+      <Box
+        width={isNonMobileScreens ? "50%" : "93%"}
+        p="2rem"
+        m="2rem auto"
+        borderRadius="1.5rem"
+        backgroundColor={theme.palette.background.alt}
+      >
+        <Typography fontWeight="500" varian="h5" sx={{ mb: "1.5rem" }}>
+          We are .getInfected please wellcome on the board!
+        </Typography>
+      </Box>
+    </Box>
+  );
 }
 
-export default LoginPage
+export default LoginPage;
