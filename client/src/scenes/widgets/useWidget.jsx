44cbbc92dafec50b240083712ce937709857fd95
Darko <dj_mamlyooo@hotmail.com>
2023-10-15 19:43:29 +0200

    userWidget Implement the 'getUser' function to fetch user data from the server and update the state.

--- client/src/scenes/widgets/useWidget.jsx
+++ client/src/scenes/widgets/useWidget.jsx
@@ -13,14 +13,45 @@ import { useSelector } from "react-redux";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-const UserWidget = ({userId, picturePath }) => {
-    const [user, setUser] = useState(null)
-    const {palette } = useTheme();
-    const navigate = useNavigate();
-    const token = useSelector((state) => state.token);
-    const dark = palette.neutral.dark;
-    const medium = palette.neutral.medium;
-    const main = palette.neutral.main;
-    
+const UserWidget = ({ userId, picturePath }) => {
+  const [user, setUser] = useState(null);
+  const { palette } = useTheme();
+  const navigate = useNavigate();
+  const token = useSelector((state) => state.token);
+  const dark = palette.neutral.dark;
+  const medium = palette.neutral.medium;
+  const main = palette.neutral.main;
 
-}
+  const getUser = async () => {
+    const response = await fetch(`http://localhost:3001/users/${userId}`, {
+      method: "GET",
+      headers: { Authorization: `Bearer ${token}` },
+    });
+    const data = await response.json();
+    setUser(data);
+  };
+  useEffect(() => {
+    getUser();
+  }, []);
+
+  if (!user) {
+    return null;
+  }
+  const {
+    firsName,
+    lastName,
+    location,
+    occupation,
+    viewedProfile,
+    impressions,
+    friends,
+  } = user;
+
+  return (
+    <WidgetWrapper>
+        <FlexBetween>
+
+        </FlexBetween>
+    </WidgetWrapper>
+  )
+};
