212c8402043b93fcc10b5a02dec69cbf1e521a9c
Darko <dj_mamlyooo@hotmail.com>
2023-10-15 19:16:25 +0200

    userWidget approaching

---
+++ client/src/scenes/widgets/useWidget.jsx
@@ -0,0 +1,15 @@
+import {
+  ManageAccountsOutlined,
+  EditOutlined,
+  LocationOutlined,
+  WrokOutlinedOutlined,
+} from "mui/icons/material";
+import { Box, Divider, useTheme } from "@mui/system";
+import Typography from "@mui/material/Typography";
+import UserImage from "components/UserImage";
+import FlexBetween from "components/FlexBetween";
+import WidgetWrapper from "components/WidgetWrapper";
+import { useSelector } from "react-redux";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
