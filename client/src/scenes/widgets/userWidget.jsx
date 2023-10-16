fc2ba0e55073f909c03033c764fe0c6163410c61
Darko <dj_mamlyooo@hotmail.com>
2023-10-16 20:35:35 +0200

    4th row completed

--- client/src/scenes/widgets/userWidget.jsx
+++ client/src/scenes/widgets/userWidget.jsx
@@ -87,6 +87,7 @@ const UserWidget = ({ userId, picturePath }) => {
             <Typography color={medium}>{occupation}</Typography>
           </Box>
         </Box>
+
         {/* 3rd row */}
         <Box p="1rem">
           <FlexBetween mb="0.5rem">
@@ -96,10 +97,43 @@ const UserWidget = ({ userId, picturePath }) => {
             </Typography>
           </FlexBetween>
           <FlexBetween>
-          <Typography color={medium}>Impressions of your post</Typography>
+            <Typography color={medium}>Impressions of your post</Typography>
             <Typography color={main} fontWeight="500">
               {impressions}
             </Typography>
+          </FlexBetween>
+        </Box>
+
+        {/* 4th row */}
+        <Box p="1rem 0">
+          <Typography fontSize="1rem" color={main} fontWeight="500" mb="1rem">
+            Social Profiles
+          </Typography>
+
+          <FlexBetween gap="1rem" mb="0.5rem">
+            <FlexBetween gap="1rem">
+              <img src="../assets/twitter.png" alt="twitter" />
+              <Box>
+                <Typography color={main} fontWeight="500">
+                  Twitter
+                </Typography>
+                <Typography color={medium}>Social Network</Typography>
+              </Box>
+            </FlexBetween>
+            <EditOutlined sx={{ color: main }}/>
+          </FlexBetween>
+
+          <FlexBetween gap="1rem" >
+            <FlexBetween gap="1rem">
+              <img src="../assets/linkedin.png" alt="linkedin" />
+              <Box>
+                <Typography color={main} fontWeight="500">
+                  Lindekin
+                </Typography>
+                <Typography color={medium}>Network Platform </Typography>
+              </Box>
+            </FlexBetween>
+            <EditOutlined sx={{ color: main }}/>
           </FlexBetween>
         </Box>
       </FlexBetween>
