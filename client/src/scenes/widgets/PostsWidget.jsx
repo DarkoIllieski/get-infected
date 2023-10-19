442155b464974a13a2f8512c2f7812b218c1989c
Darko <dj_mamlyooo@hotmail.com>
2023-10-19 12:38:59 +0200

    PostWidget state initialized

---
+++ client/src/scenes/widgets/PostsWidget.jsx
@@ -0,0 +1,10 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+const PostsWidget = ({ userId, isProfile = false }) => {
+  const dispatch = useDispatch();
+  const posts = useSelector((state) => state.posts);
+  const token = useSelector((state) => state.token);
+};
+
+export default PostsWidget;
