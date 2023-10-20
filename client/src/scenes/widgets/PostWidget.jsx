4981c0c48060b42b7fca70a86572e04ed466cd69
Darko <dj_mamlyooo@hotmail.com>
2023-10-20 16:01:50 +0200

    Implement like feature

--- client/src/scenes/widgets/PostWidget.jsx
+++ client/src/scenes/widgets/PostWidget.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Friend from "components/Friend";
 import { useDispatch, useSelector } from "react-redux";
 import { setPost } from "state";
+import { SettingsApplications } from "@mui/icons-material";
 
 const PostWidget = ({
   postId,
@@ -14,7 +15,29 @@ const PostWidget = ({
   likes,
   comments,
 }) => {
+  const [isComments, setIsComments] = useState(false);
+  const dispatch = useDispatch();
+  const token = useSelector((state) => state.token);
+  const loggedInUserId = useSelector((state) => state.user._id);
+  const isLiked = Boolean(likes[loggedInUser]);
+  const likeCount = Object.keys(likes).length;
 
+  const { palette } = useTheme();
+  const main = palette.neutral.main;
+  const primary = palette.primary.medium;
+
+  const patchLikes = async () => {
+    const response = await fetch(`http://localhost:3001/posts/${likes}/like`, {
+      method: "PATCH",
+      headers: {
+        Authorization: `Barear ${token}`,
+        "Content-type": "applications/json",
+      },
+      body: JSON.stringify({ userId: loggedInUser }),
+    });
+    const updatePost = await response.json();
+    dispatch(setPost({ post: updatePost }));
+  };
 };
 
 export default PostWidget;
