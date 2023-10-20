025ebfed57755852af579499e1399dd7f08c8203
Darko <dj_mamlyooo@hotmail.com>
2023-10-20 15:46:47 +0200

    Add PostWidget component with React state and Redux integration

---
+++ client/src/scenes/widgets/PostWidget.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import Friend from "components/Friend";
+import { useDispatch, useSelector } from "react-redux";
+import { setPost } from "state";
+
+const PostWidget = ({
+  postId,
+  postUserId,
+  name,
+  description,
+  location,
+  picturePath,
+  userPicturePath,
+  likes,
+  comments,
+}) => {
+
+};
+
+export default PostWidget;
