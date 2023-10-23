28a41bc524d92c63a3fa2cc4f40bc4629767ec7f
Darko <dj_mamlyooo@hotmail.com>
2023-10-23 14:57:09 +0200

    AdvertWidgett initialized

---
+++ client/src/components/AdvertWidget.jsx
@@ -0,0 +1,13 @@
+import { useTheme } from "@mui/material/styles";
+import Typography from "@mui/material/Typography";
+import WidgetWrapper from "./WidgetWrapper";
+import FlexBetween from "./FlexBetween";
+
+const advertWidget = () => {
+  const { palette } = useTheme();
+  const dark = palette.neutral.dark;
+  const main = palette.neutral.main;
+  const medium = palette.neutral.medium;
+
+  return;
+};
