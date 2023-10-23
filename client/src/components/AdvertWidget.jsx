5fbd9218823c4b7f362817f3bee4aa8a5bd7bc1f
Darko <dj_mamlyooo@hotmail.com>
2023-10-23 15:07:30 +0200

    AdvertWidgett hardcode displayed

--- client/src/components/AdvertWidget.jsx
+++ client/src/components/AdvertWidget.jsx
@@ -9,5 +9,21 @@ const advertWidget = () => {
   const main = palette.neutral.main;
   const medium = palette.neutral.medium;
 
-  return;
+  return (
+    <WidgetWrapper>
+        <FlexBetween>
+      <Typography color={dark} varian="h5" fontWeight="500">
+        Sponsored
+      </Typography>
+      <Typography color={medium}>Create Ad</Typography>
+      </FlexBetween>
+      <img 
+      width="100%"
+      height="auto"
+      alt="advert"
+      src="http://localhost:3001/assets/info4.jpeg"
+      style={{ borderRadius: "0.75rem", margin: "0.75rem 0" }}
+      />
+    </WidgetWrapper>
+  );
 };
