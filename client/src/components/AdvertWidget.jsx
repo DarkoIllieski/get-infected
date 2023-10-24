ee690a38603a8acb25ea58047b0dd24f75f52e03
Darko <dj_mamlyooo@hotmail.com>
2023-10-24 23:20:56 +0200

    AdvertWidgett nice and simple, finished!

--- client/src/components/AdvertWidget.jsx
+++ client/src/components/AdvertWidget.jsx
@@ -11,19 +11,26 @@ const advertWidget = () => {
 
   return (
     <WidgetWrapper>
-        <FlexBetween>
-      <Typography color={dark} varian="h5" fontWeight="500">
-        Sponsored
-      </Typography>
-      <Typography color={medium}>Create Ad</Typography>
+      <FlexBetween>
+        <Typography color={dark} varian="h5" fontWeight="500">
+          Sponsored
+        </Typography>
+        <Typography color={medium}>Create Ad</Typography>
       </FlexBetween>
-      <img 
-      width="100%"
-      height="auto"
-      alt="advert"
-      src="http://localhost:3001/assets/info4.jpeg"
-      style={{ borderRadius: "0.75rem", margin: "0.75rem 0" }}
+      <img
+        width="100%"
+        height="auto"
+        alt="advert"
+        src="http://localhost:3001/assets/info4.jpeg"
+        style={{ borderRadius: "0.75rem", margin: "0.75rem 0" }}
       />
+      <FlexBetween>
+        <Typography color={main}>Monika Consmetics</Typography>
+        <Typography color={medium}>Monikaonsmetics.com</Typography>
+      </FlexBetween>
+      <Typography color={medium} m="0.5rem 0">
+        Discover our large selection of beauty products
+      </Typography>
     </WidgetWrapper>
   );
 };
