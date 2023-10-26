9d8de12c049a272e8b3da9efe7b2cf052c69b083
Darko <dj_mamlyooo@hotmail.com>
2023-10-26 20:06:06 +0200

    FriendListWidget ready for import

--- client/src/scenes/widgets/FriendListWidget.jsx
+++ client/src/scenes/widgets/FriendListWidget.jsx
@@ -20,10 +20,33 @@ const FriendListWidget = ({ userId }) => {
       }
     );
     const data = await response.json();
-    dispatch(setFriends({ friends: data}))
+    dispatch(setFriends({ friends: data }));
   };
   useEffect(() => {
-    getFriends()
-  }, [])
-  
+    getFriends();
+  }, []);
+
+  return;
+  <WidgetWrapper>
+    <Typography
+      color={palette.neutral.dark}
+      variant="h5"
+      fontWeight="500"
+      sx={{ mb: "1.5rem" }}
+    >
+      Friend List
+    </Typography>
+    <Box display="flex" flexDirection="column" gap="1.5rem">
+      {friends.map((friend) => (
+        <Friend
+          key={friend._id}
+          friendId={friend._id}
+          name={`${friend.firstName} ${friend.lastName}`}
+          subtitle={friend.occupation}
+          userPicturePath={friend.picturePath}
+        />
+      ))}
+    </Box>
+  </WidgetWrapper>;
 };
+export default FriendListWidget;
